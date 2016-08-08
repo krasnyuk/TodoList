171140c2e9d0226a5b227db706c648eeff1afa9e
krasnyuk <krasnyuk.photo@gmail.com>
2016-08-09 02:51:17 +0300

    Update for todo lists

--- NLT/Scripts/app/TodoLists.js
+++ NLT/Scripts/app/TodoLists.js
@@ -9,11 +9,16 @@ $(document).ready(function () {
             $.each(data, function (key, item) {
                 // Add a list item for the ToDoList.
                 $('<li>', { text: formatItem(item) })
-                    .append('<button id=\"Id' + item.Id + '\" onclick="DeleteTodoList(this.id)" class="btn btn-xs btn-warning pull-right"><span class="glyphicon glyphicon-trash"></span></button>')
+                    
+                    .append('<button id=\"Id' + item.Id + '\" onclick="DeleteTodoList(this.id)" class="btn btn-xs btn-danger pull-right"><span class="glyphicon glyphicon-trash"></span></button>')
+                    .append('<button id=\"Id' + item.Id + '\" onclick="UpdateTodoList(this.id)" class="btn btn-xs btn-warning pull-right"><span class="glyphicon glyphicon-edit"></span></button>')
                     .appendTo($("#Todos"))
                     .addClass("list-group-item");
             });
-        });
+        })
+    .fail(function (jqXHR, status, error) {
+        console.log(status, error);
+    });
 
     //Creating new To-Do list
     $('#toDoCreate').click(function() { //on button click
@@ -28,21 +33,52 @@ $(document).ready(function () {
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify(toDoList),
             success: function (data, textStatus, xhr) {
-                console.log(data);
+                location.reload();
             },
             error: function (xhr, textStatus, errorThrown) {
-                console.log('Error in Operation');
+                alert('error!');
             }
         });
         });
 });
+
+function sendAjaxUpdate() {
+    var list = {
+        Id: $('#updateListId').text(),
+            Title: $('#toDoTitleUpdate').val(),
+            Date: $('#toDoDateUpdate').val()
+        };
+    $.ajax({
+        url: getToDoListUri + '/' + list.Id,
+        type: 'PUT',
+        dataType: 'json',
+        data: list,
+        success: function (data, textStatus, xhr) {
+            location.reload();
+        }
+    });
+}
+function UpdateTodoList(listId) {
+    var list = new Object();
+    $.ajax(
+        {
+            url: getToDoListUri + '/' + listId.substring(2),
+            type: 'GET',
+            success: function(data, textStatus, xhr) {
+                $('#toDoTitleUpdate').val(data.Title);
+                $('#toDoDateUpdate').val(data.Date);
+                $('#updateListId').text(data.Id);
+            }
+        }
+        );
+}
 
 function DeleteTodoList(listId) {
     var resultId = +listId.substring(2);
     $.ajax({
         url: getToDoListUri + '/' + resultId,
         type: 'DELETE',
-        dataType: 'json',
+        //dataType: 'json',
         //data: person,
         success: function (data, textStatus, xhr) {
             location.reload();
